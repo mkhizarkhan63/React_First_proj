6c323d7718464da843812fbeb6ac7b510a377aa3
Khizar Khan <46076164+mkhizarkhan63@users.noreply.github.com>
2023-02-22 00:33:06 +0500

    home4_workdone

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # React_first_proj
- 
+
+https://www.youtube.com/watch?v=b50zSyLiCYQ
+
+https://www.google.com/search?q=react+full+course+project+with+material+ui+in+hindi&biw=1455&bih=717&tbm=vid&sxsrf=AJOqlzV18_IaVon_zBTO83vFJc4rEBBM1A%3A1676989216099&ei=INP0Y8HeBfzz4-EPvIeb6AM&ved=0ahUKEwjB7pHU56b9AhX8-TgGHbzDBj0Q4dUDCA0&uact=5&oq=react+full+course+project+with+material+ui+in+hindi&gs_lcp=Cg1nd3Mtd2l6LXZpZGVvEAMyCgghEKABEMMEEAo6BAgjECdQtyNYjzNg_DRoAHAAeACAAfEDiAGKGJIBAzQtN5gBAKABAcABAQ&sclient=gws-wiz-video#fpstate=ive&vld=cid:fd91991f,vid:SzjeoauKvig
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,101 @@
-import React from 'react'
-
+import React from "react";
+import vg from "../assets/images/vg.jpeg";
+import {
+  AiFillGoogleCircle,
+  AiFillAmazonCircle,
+  AiFillYoutube,
+  AiFillInstagram,
+} from "react-icons/ai";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="home">
+        <main>
+          <h1>TechyStar</h1>
+          <p>Solution to all your problems</p>
+        </main>
+      </div>
+      <div className="home2">
+        <img src={vg} alt="" />
+        <div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+            sit, eum repellendus velit quo dignissimos quia possimus animi
+            itaque aperiam libero quisquam recusandae quod? Cumque pariatur
+          </p>
+        </div>
+      </div>
 
-export default Home
+      <div className="home3">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+            dolores reiciendis, beatae aut quas voluptas iste culpa nulla. Illum
+            exercitationem harum quos autem iusto minima rem dolor eius quae
+            porro. Neque beatae dolor cupiditate, velit amet eveniet distinctio?
+            Dolorum hic quisquam voluptatum nihil reprehenderit eos, doloremque
+            quibusdam suscipit excepturi repellendus magni. Aspernatur atque
+            ipsa accusantium harum magni voluptatibus dolore deleniti? Sequi
+            nisi explicabo laborum deleniti repellendus nam esse, corporis
+            necessitatibus laudantium facere corrupti ipsa suscipit itaque culpa
+            repudiandae fugit repellat quis eveniet quaerat nemo eos quam. Saepe
+            provident fugiat suscipit. Laboriosam asperiores amet porro rerum
+            odio delectus? In, ipsa nihil est deleniti repudiandae at minus,
+            dolor voluptatem dolorum laborum quibusdam expedita suscipit natus
+            sint alias id porro fugiat ducimus voluptas. Corrupti aliquam,
+            aspernatur libero ad totam non aperiam alias saepe unde dolore natus
+            sit hic atque dignissimos, aut cum obcaecati qui quo ea. Dolore
+            ullam cumque consequuntur nobis deleniti laborum! Quidem,
+            reprehenderit adipisci, nostrum ipsa nisi eos animi non labore vitae
+            veniam aspernatur quae nam iusto quibusdam rem possimus maxime quo
+            repellendus asperiores fuga molestiae recusandae aliquid.
+            Consequatur, fugiat esse. Esse assumenda deleniti nisi accusantium,
+          </p>
+        </div>
+      </div>
+
+      <div className="home4">
+        <div>
+          <h1>Brands</h1>
+          <article>
+            <div
+              style={{
+                animationDelay: "0.3s",
+              }}
+            >
+              <AiFillGoogleCircle />
+              <p>Google</p>
+            </div>
+            <div
+              style={{
+                animationDelay: "0.5s",
+              }}
+            >
+              <AiFillAmazonCircle />
+              <p>Amazon</p>
+            </div>
+            <div
+              style={{
+                animationDelay: "0.7s",
+              }}
+            >
+              <AiFillYoutube />
+              <p>Youtube</p>
+            </div>
+            <div
+              style={{
+                animationDelay: "0.9s",
+              }}
+            >
+              <AiFillInstagram />
+              <p>Instagram</p>
+            </div>
+          </article>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
